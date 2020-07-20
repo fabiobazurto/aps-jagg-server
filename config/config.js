62f625a513449c31c88e9f4c92ac1fb5ff69597e
Fabio Bazurto <fabio.bazurto@gmail.com>
2020-07-19 22:32:07 -0500

    package update and config adding ENV

--- config/config.js
+++ config/config.js
@@ -3,10 +3,10 @@ require('dotenv').config()
 
 //Fragmento de base de datos
 const dbConnect = {
-    "username": "root",
-    "password": "kame123+",
-    "database": "jagg_db",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
 }
 
